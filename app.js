adc05fb248dc90a9508fbcd19e8ff38e10fda9ac
seth mcfeeters <seth.mcfeeters@gmail.com>
2020-08-19 16:42:47 -0700

    added javascript

---
+++ app.js
@@ -0,0 +1,19 @@
+'use strict'
+
+var username = prompt('Please Enter Your Name: ');
+
+alert('Hello ' + username);
+
+if (username === 'maddy'){
+    document.write('<h2>' + 'Welcome Beautiful' + '</h2>');
+} else {
+    document.write( '<h2>' + 'welcome ' +username + '</h2>');
+}
+
+var food = prompt('how many times have you fed cedar today?: ');
+
+if (food === '4'){
+    document.write('<h2>' + 'Cedar is a well fed dog' + '</h2>');
+} else {
+    document.write( '<h2>' + username + ', Cedar might be hungry'+ '</h2>');
+}
